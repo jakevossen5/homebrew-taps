51b534a37984d80d8618d03d362930d65c545971
Jake Vossen <jake@vossen.dev>
2019-07-07 17:45:00 -0600

    trying to add dependencies

--- Formula/omnifocus-graph-creator.rb
+++ Formula/omnifocus-graph-creator.rb
@@ -7,7 +7,14 @@ class OmnifocusGraphCreator < Formula
   url "https://github.com/jakevossen5/omnifocus-graph-creator/archive/1.0.4.tar.gz"
   sha256 "d7cc20c7fd178a7166c5e32f23c59bbbd2c9bbe6089a1c514396ac76f9c3fd10"
   version "1.0.4"
-  # depends_on "cmake" => :build
+  
+  depends_on "python"
+  
+  
+  resource "matplotlib" do
+  url "https://files.pythonhosted.org/packages/51/fe/84ab101f8ab543d89b6a128326f62adcdafd2781ab8362a737e6ce78eea7/matplotlib-3.1.0.tar.gz"
+  sha256 "1e0213f87cc0076f7b0c4c251d7e23601e2419cd98691df79edb95517ba06f0c"
+end
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
