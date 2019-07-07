db92f0a33b71714cc1580393be396e94d23554c8
= <jake@vossen.dev>
2019-07-07 15:44:16 -0600

    added omnifocus graph creator

---
+++ Formula/omnifocus-graph-creator.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class OmnifocusGraphCreator < Formula
+  desc "Creates graphs to analyze your Omnifocus task completion history"
+  homepage "https://github.com/jakevossen5/omnifocus-graph-creator"
+  url "https://github.com/jakevossen5/omnifocus-graph-creator/archive/1.0.0.tar.gz"
+  sha256 "a17a34c71b42fff952c33c5ac21e240d100cdcba125c9b8400e16ae823db2aa3"
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # system "./configure", "--disable-debug",
+    #                       "--disable-dependency-tracking",
+    #                       "--disable-silent-rules",
+    #                       "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test omnifocus-graph-creator`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
