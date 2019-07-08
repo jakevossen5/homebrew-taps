f76f33d155cfd771cfb047cb4a268be8dc7a8572
Jake Vossen <jake@vossen.dev>
2019-07-07 20:59:50 -0600

    add numpy

--- Formula/omnifocus-graph-creator.rb
+++ Formula/omnifocus-graph-creator.rb
@@ -11,20 +11,19 @@ class OmnifocusGraphCreator < Formula
   
   depends_on "python"
   
-  
+    resource "numpy" do
+        url "https://files.pythonhosted.org/packages/d3/4b/f9f4b96c0b1ba43d28a5bdc4b64f0b9d3fbcf31313a51bc766942866a7c7/numpy-1.16.4.zip"
+        sha256 "7242be12a58fec245ee9734e625964b97cf7e3f2f7d016603f9e56660ce479c7"
+    end
   resource "matplotlib" do
     url "https://files.pythonhosted.org/packages/12/d1/7b12cd79c791348cb0c78ce6e7d16bd72992f13c9f1e8e43d2725a6d8adf/matplotlib-3.1.1.tar.gz"
     sha256 "1febd22afe1489b13c6749ea059d392c03261b2950d1d45c17e3aed812080c93"
     end
-  resource "Pillow" do
-      url "https://files.pythonhosted.org/packages/51/fe/18125dc680720e4c3086dd3f5f95d80057c41ab98326877fc7d3ff6d0ee5/Pillow-6.1.0.tar.gz"
-      sha256 "0804f77cb1e9b6dbd37601cee11283bba39a8d44b9ddb053400c58e0c0d7d9de"
-    end
 
   def install
       venv = virtualenv_create(libexec, "python3")
+      venv.pip_install "numpy"
       venv.pip_install "matplotlib"
-      venv.pip_install "Pillow"
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     # system "./configure", "--disable-debug",
