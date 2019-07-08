9ecf9a334d91c40e12447610648d03201b7cdd04
= <jake@vossen.dev>
2019-07-07 22:11:48 -0600

    added matplotlib again

--- Formula/omnifocus-graph-creator.rb
+++ Formula/omnifocus-graph-creator.rb
@@ -9,6 +9,14 @@ class OmnifocusGraphCreator < Formula
   sha256 "8f92ea5c3c7f7e446cd278182722223c0498d5cccc1a87d2388d91f236e25223"
   version "1.0.7"
   
+  resource "matplotlib" do
+  url "https://files.pythonhosted.org/packages/51/fe/84ab101f8ab543d89b6a128326f62adcdafd2781ab8362a737e6ce78eea7/matplotlib-3.1.0.tar.gz"
+  url "https://files.pythonhosted.org/packages/12/d1/7b12cd79c791348cb0c78ce6e7d16bd72992f13c9f1e8e43d2725a6d8adf/matplotlib-3.1.1.tar.gz"
+  sha256 "1e0213f87cc0076f7b0c4c251d7e23601e2419cd98691df79edb95517ba06f0c"
+  sha256 "1febd22afe1489b13c6749ea059d392c03261b2950d1d45c17e3aed812080c93"
+end
+
+  
   depends_on "python"
   depends_on "freetype"
 
