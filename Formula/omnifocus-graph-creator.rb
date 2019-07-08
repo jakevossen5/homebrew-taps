1414b4c6173a9832fe105ce85c61c8f3f25ff4cb
= <jake@vossen.dev>
2019-07-07 20:25:30 -0600

    mvoe  back to matplotlib 3.1.0, not sure why 3.1.1 isn't working

--- Formula/omnifocus-graph-creator.rb
+++ Formula/omnifocus-graph-creator.rb
@@ -13,8 +13,8 @@ class OmnifocusGraphCreator < Formula
   
   
   resource "matplotlib" do
-  url "https://files.pythonhosted.org/packages/12/d1/7b12cd79c791348cb0c78ce6e7d16bd72992f13c9f1e8e43d2725a6d8adf/matplotlib-3.1.1.tar.gz"
-  sha256 "1febd22afe1489b13c6749ea059d392c03261b2950d1d45c17e3aed812080c93"
+  url "https://files.pythonhosted.org/packages/51/fe/84ab101f8ab543d89b6a128326f62adcdafd2781ab8362a737e6ce78eea7/matplotlib-3.1.0.tar.gz"
+  sha256 "1e0213f87cc0076f7b0c4c251d7e23601e2419cd98691df79edb95517ba06f0c"
 end
 
   def install
