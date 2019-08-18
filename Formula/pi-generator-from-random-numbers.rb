6a9cc01b00d34c7cb144c1e0eba5338afa38958a
= <jake@vossen.dev>
2019-08-18 14:38:03 -0600

    pi-generator-from-random-numbers 1.0.0

---
+++ Formula/pi-generator-from-random-numbers.rb
@@ -0,0 +1,36 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class PiGeneratorFromRandomNumbers < Formula
+  desc "Generates Pi from random numbers and co-primes"
+  homepage "https://github.com/jakevossen5/pi-generator-from-random-numbers"
+  url "https://github.com/jakevossen5/pi-generator-from-random-numbers/releases/download/1.0.0/pi-generator-from-random-numbers"
+  sha256 "1c1923cd19b8e6e988c4f6f5b090f472c3f208a7d5b31b8cd185678639242480"
+  version "1.0.0"
+  
+  def install
+      bin.install "pi-generator-from-random-numbers"
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # system "./configure", "--disable-debug",
+    #                       "--disable-dependency-tracking",
+    #                       "--disable-silent-rules",
+    #                       "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    #  system "make", "prepare-for-install" # if this fails, try separate make/make install steps
+    #bin.install "omnifocus-graph-creator"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test omnifocus-graph-creator`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
