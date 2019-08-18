0d18a9d929761891ed8929af480a85958bcca318
= <jake@vossen.dev>
2019-08-18 12:14:20 -0600

    fixed sha

--- Formula/check-for-homebrew-alternatives.rb
+++ Formula/check-for-homebrew-alternatives.rb
@@ -6,7 +6,7 @@ class CheckForHomebrewAlternatives < Formula
   desc "Searches for homebrew versions of apps installed from other sources"
   homepage "https://github.com/jakevossen5/check-for-homebrew-alternatives"
   url "https://github.com/jakevossen5/check-for-homebrew-alternatives/archive/1.0.4.tar.gz"
-  sha256 "ce391567b2510d47b939e9d2be69414117a91afdef3f33711bb03d16914a4b6c"
+  sha256 "036101a6b55477abd606d691dfe8e4a2742abba23de523733c7739f52e1ee21a"
   version "1.0.4"
 
   depends_on "python"
