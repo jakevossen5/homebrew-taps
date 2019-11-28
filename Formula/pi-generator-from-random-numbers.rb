0801d5b266ce7738578eaf6516c8907a235d609e
Jake Vossen <jake@vossen.dev>
2019-11-27 21:59:40 -0700

    pi generator from random blocks 2.0.2

--- Formula/pi-generator-from-random-numbers.rb
+++ Formula/pi-generator-from-random-numbers.rb
@@ -4,9 +4,9 @@
 class PiGeneratorFromRandomNumbers < Formula
   desc "Generates Pi from random numbers and co-primes"
   homepage "https://github.com/jakevossen5/pi-generator-from-random-numbers"
-  url "https://github.com/jakevossen5/pi-generator-from-random-numbers/releases/download/2.0.1/pi-generator-from-random-numbers"
-  sha256 "69e9207ea6deb623266583ae0420ad7c5891deac1bfa18c515e0bd0d65112942"
-  version "2.0.1"
+  url "https://github.com/jakevossen5/pi-generator-from-random-numbers/releases/download/2.0.2/pi-generator-from-random-numbers"
+  sha256 "5020e8da9257916c77a2970d11be26175c9399849869644ff60fe9c8e9b69c84"
+  version "2.0.2"
   
   def install
       bin.install "pi-generator-from-random-numbers"
