303109606436f57ee9d6baad84b3d38a6dd4622b
Jake Vossen <jake@vossen.dev>
2020-09-02 19:14:20 -0600

    fixed version number

--- Formula/watson-vis.rb
+++ Formula/watson-vis.rb
@@ -4,7 +4,7 @@
 class WatsonVis < Formula
   desc "Turns Watson time data into a CLI pie chart"
   homepage "https://github.com/jakevossen5/watson-vis"
-  url "https://github.com/jakevossen5/watson-vis/releases/download/0.1.12/watson-vis"
+  url "https://github.com/jakevossen5/watson-vis/releases/download/0.1.2/watson-vis"
   sha256 "42a376600bf94c83d329bf09ebe97bcdb5e72bf3621e78c7a6ee44fa67a49105"
   version "0.1.2"
 
