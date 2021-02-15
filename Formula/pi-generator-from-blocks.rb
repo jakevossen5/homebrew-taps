26200d998c6d794053bf5392b7fe2b1aa4aea5a2
Jake Vossen <jake@vossen.dev>
2021-02-14 23:50:47 -0700

    pi generator from blocks update

--- Formula/pi-generator-from-blocks.rb
+++ Formula/pi-generator-from-blocks.rb
@@ -1,9 +1,9 @@
 class PiGeneratorFromBlocks < Formula
   desc "Generate pi in a ridiculous way using elastic collisions"
   homepage "https://github.com/jakevossen5/pi-generator-from-blocks"
-  url "https://github.com/jakevossen5/pi-generator-from-blocks/releases/download/1.0.1/pi-generator-from-blocks"
-  version "1.0.1"
-  sha256 "1f5a5b9ade1ebfa4eb978e05cfc1d82a3fb5c3612065cd67407c09a7d87d6d3f"
+  url "https://github.com/jakevossen5/pi-generator-from-blocks/releases/download/1.0.2/pi-generator-from-blocks"
+  version "1.0.2"
+  sha256 "4539bfda78eea4b91282f57913276666aaa614044100dc4560f89231b7b0a235"
 
   def install
     bin.install "pi-generator-from-blocks"
